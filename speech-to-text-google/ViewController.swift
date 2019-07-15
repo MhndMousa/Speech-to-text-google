8cfc588604e517799c6edc85e20e7f885a9d08e6
Muhannad Alnemer <mhndmsa@gmail.com>
2019-07-14 20:43:42 -0700

    Initial Commit

---
+++ speech-to-text-google/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speech-to-text-google
+//
+//  Created by Muhannad Alnemer on 7/14/19.
+//  Copyright © 2019 Muhannad Alnemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
